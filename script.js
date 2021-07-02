c8e78200831e822e61ee68c08f7bcfcc05bc0dd4
Arjun <arjun7.saili@gmail.com>
2021-07-01 20:30:35 -0400

    Added Ability for user to add books.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const myLibrary = [];
 const cardGrid = document.querySelector('.card-grid');
+const addBookButton = document.querySelector('.book-add');
+const addBookModal = document.querySelector('.add-book-modal');
+const behindModalBackground = document.querySelector('.behind-modal');
+const closeModalButton = document.querySelector('#close');
+const titleField = document.querySelector('#add-book-title');
+const authorField = document.querySelector('#add-book-author');
+const pagesField =  document.querySelector('#add-book-pages');
+const readSelect = document.querySelector('#add-book-read');
+const submitAddBook = document.querySelector('#submit');
+const allFieldElements = document.querySelectorAll('.input-field')
 function Book(title, author, numOfPages, read) {
     this.title = title;
     this.author = author;
@@ -43,7 +53,6 @@ function createBookCardInfo(currentBook){
 }
 
 function createBookCard(currentBook) {
-    console.log(currentBook);
     let newBookCard = document.createElement('div');
     newBookCard.classList.add('book-card');
     let bookCardInfo = document.createElement('div');
@@ -57,18 +66,40 @@ function createBookCard(currentBook) {
 }
 
 function addBookToLibrary(title, author, numOfPages, read) {
+    console.log(read);
     let newBook = new Book(title, author, numOfPages, read);
     myLibrary.push(newBook);
+    createBookCard(newBook)
+    console.log(myLibrary);
 }
 
-function createBookDisplay() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        createBookCard(myLibrary[i]);
+function displayAddBookModal(){
+    addBookModal.style.transition = 'opacity 0.5s';
+    addBookModal.style.opacity = '100%';
+    document.body.style.backgroundColor = 'rgb(211, 211, 211)';
+    addBookModal.style.backgroundColor = 'white';
+    behindModalBackground.style.filter = 'blur(5px)';
+}
+
+function closeAddBookModal(){
+    addBookModal.style.opacity = '0%';
+    document.body.style.backgroundColor = 'white';
+    behindModalBackground.style.filter = 'blur(0px)';
+}
+
+function addBookCard(){
+    if (readSelect.checked){
+        addBookToLibrary(titleField.value, authorField.value, pagesField.value, true);
     }
+    else{
+        addBookToLibrary(titleField.value, authorField.value, pagesField.value, false);
+    }
+    closeAddBookModal();
 }
 
 
+addBookButton.addEventListener('click', displayAddBookModal.bind(null,addBookButton));
+submitAddBook.addEventListener('click',addBookCard.bind(null,submitAddBook));
+closeModalButton.addEventListener('click', closeAddBookModal.bind(null,closeModalButton));
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
-createBookDisplay();
